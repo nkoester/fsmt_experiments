66c4773d313aba1824fbe65256a76ec111333bce
flier <flier@techfak.uni-bielefeld.de>
2013-11-30 20:49:23 +0100

    here we go, beta

--- MORSE-find-human/gplot.sh
+++ MORSE-find-human/gplot.sh
@@ -3,8 +3,7 @@
 /usr/bin/gnuplot <<__EOF
 set title '$2'
 set datafile sep ','
-# set xrange [0:200]
-# set yrange [0:200]
+set style line 1 linewidth 3
 set term png
 set output '$1.png'
 plot '$1' u 4:1 w lines title '$3'
