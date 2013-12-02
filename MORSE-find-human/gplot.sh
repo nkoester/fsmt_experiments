9db169bf408f56bb6b0b02c774a8504a1b06d7c9
hudsonservice <hudsonservice@samarium.(none)>
2013-12-02 16:31:37 +0100

    axis label

--- MORSE-find-human/gplot.sh
+++ MORSE-find-human/gplot.sh
@@ -2,9 +2,11 @@
 
 /usr/bin/gnuplot <<__EOF
 set title '$2'
+set xlabel "Time in seconds"
+set ylabel "Position in meters"
 set datafile sep ','
 set style line 1 linewidth 3
 set term png
-set output '$1.png'
+set output '$WORKSPACE/$FSMTRA.png'
 plot '$1' u 4:1 w lines title '$3'
 __EOF
